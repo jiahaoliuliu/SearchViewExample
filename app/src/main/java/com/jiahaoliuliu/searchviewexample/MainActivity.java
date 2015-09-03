9bc6d02bd1dca0cc80c24fd8e64389f17fd690ef
Jiahao Liu Liu <jiahaoliuliu@gmail.com>
2015-09-03 22:44:44 +0400

    Added the search icon and search layout

--- app/src/main/java/com/jiahaoliuliu/searchviewexample/MainActivity.java
+++ app/src/main/java/com/jiahaoliuliu/searchviewexample/MainActivity.java
@@ -2,6 +2,8 @@ package com.jiahaoliuliu.searchviewexample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -9,6 +11,9 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private Button mExpandSearchViewButton;
+
+    private MenuItem mMenuItemSearch;
+    private static final int MENU_ITEM_SEARCH_ID = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +38,17 @@ public class MainActivity extends AppCompatActivity {
     private void expandSearchView() {
         Toast.makeText(this, "Expanding the search view", Toast.LENGTH_SHORT).show();
         // TODO: implement this
+    }
+
+    // Action bar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Update user profile
+        mMenuItemSearch = menu.add(Menu.NONE, MENU_ITEM_SEARCH_ID, Menu
+                .NONE, R.string.action_bar_search)
+                .setIcon(R.drawable.ic_action_search)
+                .setActionView(R.layout.search_layout);
+        mMenuItemSearch.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM | MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW);
+        return true;
     }
 }
